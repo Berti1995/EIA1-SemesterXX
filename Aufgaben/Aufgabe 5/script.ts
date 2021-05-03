db95c0ba303a9bc93c7bfbaa2209809d6c564da2
Berti1995 <80836560+Berti1995@users.noreply.github.com>
2021-05-03 11:23:34 +0200

    Aufgabe 6 bearbeitet

--- Aufgaben/Aufgabe 5/script.ts
+++ Aufgaben/Aufgabe 5/script.ts
@@ -1,76 +1,114 @@
-var africa: string ="Africa";
-var asia: string ="Asia";
-var australia: string ="Australia";
-var europe: string ="Europe";
-var northamerica: string ="Northamerica";
-var southamerica: string ="Southamerica";
-
-var africa_2008: number = 1028;
-var africa_2018: number = 1235.5;
-var asia_2008: number = 12954.7;
-var asia_2018: number = 16274.1;
-var australia_2008: number = 1993;
-var australia_2018: number = 2100.5;
-var europe_2008: number = 4965.7;
-var europe_2018: number = 4209.3;
-var northamerica_2008: number = 6600.4;
-var northamerica_2018: number = 6035.6;
-var southamerica_2008: number = 1132.6;
-var southamerica_2018: number = 1261.5;
-
-var wholeworld_2018 = africa_2018 + asia_2018 + australia_2018 + northamerica_2018 + southamerica_2018 + europe_2018;
-
-var europe_whole2018 = europe_2018 / wholeworld_2018 * 100;
-var asia_whole2018 = asia_2018 / wholeworld_2018 * 100;
-var australia_whole2018 = australia_2018 / wholeworld_2018 * 100;
-var africa_whole2018 = africa_2018 / wholeworld_2018 * 100;
-var northamerica_whole2018 = northamerica_2018 / wholeworld_2018 * 100;
-var southamerica_whole2018 = southamerica_2018 / wholeworld_2018 * 100;
-
-var europe08_18kg = europe_2018-europe_2008;
-var asia08_18kg = asia_2018-asia_2008;
-var australia08_18kg = australia_2018-australia_2008;
-var africa08_18kg = africa_2018-africa_2008;
-var northamerica08_18kg = northamerica_2018-northamerica_2008;
-var southamerica08_18kg = southamerica_2018-southamerica_2008;
-
-var europe08_18pro = europe08_18kg / europe_2008 *100;
-var asia08_18pro = asia08_18kg / asia_2008 *100;
-var australia08_18pro = australia08_18kg / australia_2008 *100;
-var africa08_18pro = africa08_18kg / africa_2008 *100;
-var northamerica08_18pro = northamerica08_18kg / northamerica_2008 *100;
-var southamerica08_18pro = southamerica08_18kg / southamerica_2008 *100;
-
-console.log("Die Emission von Europa ist:" + europe_2018 + "kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit" + roundTwo (europe_whole2018) +  "%");
-console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um" + roundTwo(europe08_18pro) + "% verändert");
-console.log("2018 im Vergleich zu 2008 sind das" + roundTwo(europe08_18kg) + "kg CO2");
-
-console.log("Die Emission von asia ist:" + asia_2018 + "kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht asia damit" + roundTwo (asia_whole2018) +  "%");
-console.log("Für asia hat sich 2018 im Vergleich zu 2008 die Emission um" + roundTwo(asia08_18pro) + "% verändert");
-console.log("2018 im Vergleich zu 2008 sind das" + roundTwo(asia08_18kg) + "kg CO2");
-
-console.log("Die Emission von australia ist:" + australia_2018 + "kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht australia damit" + roundTwo (australia_whole2018) +  "%");
-console.log("Für australia hat sich 2018 im Vergleich zu 2008 die Emission um" + roundTwo(australia08_18pro) + "% verändert");
-console.log("2018 im Vergleich zu 2008 sind das" + roundTwo(australia08_18kg) + "kg CO2");
-
-console.log("Die Emission von africa ist:" + africa_2018 + "kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht africa damit" + roundTwo (africa_whole2018) +  "%");
-console.log("Für africa hat sich 2018 im Vergleich zu 2008 die Emission um" +  roundTwo(africa08_18pro) + "% verändert");
-console.log("2018 im Vergleich zu 2008 sind das" + roundTwo(africa08_18kg) + "kg CO2");
-
-console.log("Die Emission von northamerica ist:" + northamerica_2018 + "kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht northamerica damit" + roundTwo (northamerica_whole2018) + "%");
-console.log("Für northamerica hat sich 2018 im Vergleich zu 2008 die Emission um" + roundTwo(northamerica08_18pro) + "% verändert");
-console.log("2018 im Vergleich zu 2008 sind das" + roundTwo(northamerica08_18kg) + "kg CO2");
-
-console.log("Die Emission von southamerica ist:" + southamerica_2018 + "kg CO2");
-console.log("Relativ zur Gesamtemission der Welt verursacht southamerica damit" + roundTwo (southamerica_whole2018) + "%");
-console.log("Für southamerica hat sich 2018 im Vergleich zu 2008 die Emission um" + roundTwo(southamerica08_18pro) + "% verändert");
-console.log("2018 im Vergleich zu 2008 sind das"+ roundTwo(southamerica08_18kg) + "kg CO2");
-
 function roundTwo(x: number): number{
     return Math.round(x * 100) / 100;
 }
+
+var africa_2008: number = 1028;
+var africa_2018: number = 1235.5;
+var africa: string ="Africa";
+
+
+var asia_2008: number = 12954.7;
+var asia_2018: number = 16274.1;
+var asia: string ="Asia";
+
+
+var australia_2008: number = 1993;
+var australia_2018: number = 2100.5;
+var australia: string ="Australia";
+
+
+var europe_2008: number = 4965.7;
+var europe_2018: number = 4209.3;
+var europe: string ="Europe";
+
+
+var northamerica_2008: number = 6600.4;
+var northamerica_2018: number = 6035.6;
+var northamerica: string ="Northamerica";
+
+
+var southamerica_2008: number = 1132.6;
+var southamerica_2018: number = 1261.5;
+var southamerica: string ="Southamerica";
+
+
+var wholeworld_2018 = africa_2018 + asia_2018 + australia_2018 + northamerica_2018 + southamerica_2018 + europe_2018;
+
+document.querySelector("h2").id = "Text";
+
+
+
+function titelEurope() {
+
+    document.querySelector("#titleRegion").innerHTML = "Europe";
+    document.querySelector(".H2eins").innerHTML = + europe_2018 + "";
+    document.querySelector(".H2zwei").innerHTML = + roundTwo(europe_2018 / wholeworld_2018 *100) + "%";
+    document.querySelector(".H2drei").innerHTML = + roundTwo((europe_2018 - europe_2008) / europe_2008 * 100) + "%";
+    document.querySelector(".H2vier").innerHTML = + roundTwo(europe_2018 - europe_2008) + "";
+    document.querySelector(".chart").setAttribute("style", "height: 13.53%");
+    document.querySelector("body").setAttribute("style", "color: blue");
+    
+
+}
+document.querySelector(".europe").addEventListener("click", titelEurope);
+
+
+function titelAfrica() {
+
+    document.querySelector("#titleRegion").innerHTML = "Africa";
+    document.querySelector(".H2eins").innerHTML = + africa_2018 + "";
+    document.querySelector(".H2zwei").innerHTML = + roundTwo(africa_2018 / wholeworld_2018 *100) + "%";
+    document.querySelector(".H2drei").innerHTML = + roundTwo((africa_2018 - africa_2008) / africa_2008 * 100) + "%";
+    document.querySelector(".H2vier").innerHTML = + roundTwo(africa_2018 - africa_2008) + "";
+    document.querySelector(".chart").setAttribute("style", "height: 3.97%");
+    document.querySelector("body").setAttribute("style", "color: red");
+}
+document.querySelector(".africa").addEventListener("click", titelAfrica);
+
+function titelAsia() {
+
+    document.querySelector("#titleRegion").innerHTML = "Asia";
+    document.querySelector(".H2eins").innerHTML = + asia_2018 + "";
+    document.querySelector(".H2zwei").innerHTML = + roundTwo(asia_2018 / wholeworld_2018 *100) + "%";
+    document.querySelector(".H2drei").innerHTML = + roundTwo((asia_2018 - asia_2008) / asia_2008 * 100) + "%";
+    document.querySelector(".H2vier").innerHTML = + roundTwo(asia_2018 - asia_2008) + "";
+    document.querySelector(".chart").setAttribute("style", "height: 52.3%");
+    document.querySelector("body").setAttribute("style", "color: yellow");
+}
+document.querySelector(".asia").addEventListener("click", titelAsia);
+
+function titelAustralia() {
+
+    document.querySelector("#titleRegion").innerHTML = "Australia";
+    document.querySelector(".H2eins").innerHTML = + australia_2018 + "";
+    document.querySelector(".H2zwei").innerHTML = + roundTwo(australia_2018 / wholeworld_2018 *100) + "%";
+    document.querySelector(".H2drei").innerHTML = + roundTwo((australia_2018 - australia_2008) / australia_2008 * 100) + "%";
+    document.querySelector(".H2vier").innerHTML = + roundTwo(australia_2018 - australia_2008) + "";
+    document.querySelector(".chart").setAttribute("style", "height: 6.75%");
+    document.querySelector("body").setAttribute("style", "color: white");
+}
+document.querySelector(".australia").addEventListener("click", titelAustralia);
+
+function titelNorthamerica() {
+
+    document.querySelector("#titleRegion").innerHTML = "Northamerica";
+    document.querySelector(".H2eins").innerHTML = + northamerica_2018 + "";
+    document.querySelector(".H2zwei").innerHTML = + roundTwo(northamerica_2018 / wholeworld_2018 *100) + "%";
+    document.querySelector(".H2drei").innerHTML = + roundTwo((northamerica_2018 - northamerica_2008) / northamerica_2008 * 100) + "%";
+    document.querySelector(".H2vier").innerHTML = + roundTwo(northamerica_2018 - northamerica_2008) + "";
+    document.querySelector(".chart").setAttribute("style", "height: 19.4%");
+    document.querySelector("body").setAttribute("style", "color: black");
+}
+document.querySelector(".northamerica").addEventListener("click", titelNorthamerica);
+
+function titelSouthamerica() {
+
+    document.querySelector("#titleRegion").innerHTML = "Southamerica";
+    document.querySelector(".H2eins").innerHTML = + southamerica_2018 + "";
+    document.querySelector(".H2zwei").innerHTML = + roundTwo(southamerica_2018 / wholeworld_2018 *100) + "%";
+    document.querySelector(".H2drei").innerHTML = + roundTwo((southamerica_2018 - southamerica_2008) / southamerica_2008 * 100) + "%";
+    document.querySelector(".H2vier").innerHTML = + roundTwo(southamerica_2018 - southamerica_2008) + "";
+    document.querySelector(".chart").setAttribute("style", "height: 4.05%");
+    document.querySelector("body").setAttribute("style", "color: purple");
+}
+document.querySelector(".southamerica").addEventListener("click", titelSouthamerica);
