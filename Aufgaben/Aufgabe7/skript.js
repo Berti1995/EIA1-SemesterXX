b19467114452b3157584be6d0f4bb9f4bad00e1f
Berti1995 <80836560+Berti1995@users.noreply.github.com>
2021-05-18 15:49:19 +0200

    Aufgabe 8 (sry für die Verspätung)

--- Aufgaben/Aufgabe7/skript.js
+++ Aufgaben/Aufgabe7/skript.js
@@ -1,9 +1,23 @@
-//7.1//
+var loop = setInterval(function () { }, 1000);
+var randomButton = setInterval(function () { }, 500);
+var stopBtn = document.querySelector(".stop");
+var playBtn = document.querySelector(".playbutton");
+var indexZahler = 0;
+var alle = [
+    new Audio("assets/A.mp3"),
+    new Audio("assets/A.mp3"),
+    new Audio("assets/C.mp3"),
+    new Audio("assets/F.mp3"),
+    new Audio("assets/hihat.mp3"),
+    new Audio("assets/kick.mp3"),
+    new Audio("assets/laugh-1.mp3"),
+    new Audio("assets/laugh-2.mp3"),
+    new Audio("assets/snare.mp3"),
+];
+function playMp3(sound) {
+    sound.play();
+}
 window.addEventListener('load', function () {
-    var alle = [new Audio("assets/A.mp3"), new Audio("assets/A.mp3"), new Audio("assets/C.mp3"), new Audio("assets/F.mp3"), new Audio("assets/hihat.mp3"), new Audio("assets/kick.mp3"), new Audio("assets/laugh-1.mp3"), new Audio("assets/laugh-2.mp3"), new Audio("assets/snare.mp3"),];
-    function playMp3(sound) {
-        sound.play();
-    }
     document.querySelector(".button1").addEventListener("click", function () { playMp3(alle[0]); });
     document.querySelector(".button2").addEventListener("click", function () { playMp3(alle[1]); });
     document.querySelector(".button3").addEventListener("click", function () { playMp3(alle[2]); });
@@ -13,30 +27,41 @@ window.addEventListener('load', function () {
     document.querySelector(".button7").addEventListener("click", function () { playMp3(alle[8]); });
     document.querySelector(".button8").addEventListener("click", function () { playMp3(alle[6]); });
     document.querySelector(".button9").addEventListener("click", function () { playMp3(alle[7]); });
-    //7.2//
-    document.querySelector(".playbutton").addEventListener("click", function () { playbeat(); });
-    function playbeat() {
-        setTimeout(function () {
-            playMp3(alle[5]);
-        }, 500);
-        setTimeout(function () {
-            playMp3(alle[4]);
-        }, 750);
-        setTimeout(function () {
-            playMp3(alle[8]);
-        }, 1000);
-        setTimeout(function () {
-            playMp3(alle[5]);
-        }, 1250);
-        setTimeout(function () {
-            playMp3(alle[4]);
-        }, 1500);
-        setTimeout(function () {
-            playMp3(alle[8]);
-        }, 2000);
-        setTimeout(function () {
-            playMp3(alle[7]);
-        }, 2250);
-    }
 });
+// playbutton//
+document.querySelector(".playbutton").addEventListener("click", function () { playbeat(); });
+function playbeat() {
+    loop = setInterval(function () {
+        setTimeout(function () { playMp3(alle[5]); }, 500);
+        setTimeout(function () { playMp3(alle[4]); }, 750);
+        setTimeout(function () { playMp3(alle[8]); }, 1000);
+    }, 1000);
+}
+//random beat//
+document.querySelector(".random").addEventListener("click", function () { random(); });
+function random() {
+    randomButton = setInterval(function () {
+        playMp3(alle[Math.floor(Math.random() * 9)]);
+        console.log(randomButton);
+    }, 500);
+}
+//delete button//
+document.querySelector(".delete").addEventListener("click", function () { deleteRemix(); });
+function deleteRemix() {
+    clearInterval(randomButton);
+    clearInterval(loop);
+}
+//play/stop funktion//
+playBtn.addEventListener("click", function () {
+    changeClasses(this, stopBtn);
+});
+stopBtn.addEventListener("click", function () {
+    changeClasses(this, playBtn);
+    clearInterval(loop);
+    indexZahler = 0;
+});
+function changeClasses(firsHTMLElement, secondHTMLElement) {
+    firsHTMLElement.classList.add("isHidden");
+    secondHTMLElement.classList.remove("isHidden");
+}
 //# sourceMappingURL=skript.js.map
--- Aufgaben/Aufgabe7/skript.ts
+++ Aufgaben/Aufgabe7/skript.ts
@@ -1,57 +1,85 @@
-//7.1//
-window.addEventListener('load', function () {
 
-let alle: HTMLAudioElement[] = [new Audio("assets/A.mp3"), new Audio("assets/A.mp3"), new Audio("assets/C.mp3"), new Audio("assets/F.mp3"), new Audio("assets/hihat.mp3"), new Audio("assets/kick.mp3"), new Audio("assets/laugh-1.mp3"), new Audio("assets/laugh-2.mp3"), new Audio("assets/snare.mp3"),];
+var loop: number = setInterval(function () {}, 1000);
+var randomButton: number = setInterval(function () {}, 500);
+var stopBtn: HTMLElement = document.querySelector(".stop");
+var playBtn: HTMLElement = document.querySelector(".playbutton");
+var indexZahler: number = 0;
 
-function playMp3(sound: HTMLAudioElement) {
+
+var alle: any = [
+new Audio("assets/A.mp3"), 
+new Audio("assets/A.mp3"), 
+new Audio("assets/C.mp3"), 
+new Audio("assets/F.mp3"), 
+new Audio("assets/hihat.mp3"), 
+new Audio("assets/kick.mp3"), 
+new Audio("assets/laugh-1.mp3"), 
+new Audio("assets/laugh-2.mp3"), 
+new Audio("assets/snare.mp3"),
+];
+
+
+function playMp3(sound: HTMLAudioElement): void {
 
     sound.play();
 }
+window.addEventListener('load', function () {
 
-document.querySelector(".button1").addEventListener("click", function () { playMp3(alle[0]) });
-document.querySelector(".button2").addEventListener("click", function () { playMp3(alle[1]) });
-document.querySelector(".button3").addEventListener("click", function () { playMp3(alle[2]) });
-document.querySelector(".button4").addEventListener("click", function () { playMp3(alle[3]) });
-document.querySelector(".button5").addEventListener("click", function () { playMp3(alle[4]) });
-document.querySelector(".button6").addEventListener("click", function () { playMp3(alle[5]) });
-document.querySelector(".button7").addEventListener("click", function () { playMp3(alle[8]) });
-document.querySelector(".button8").addEventListener("click", function () { playMp3(alle[6]) });
-document.querySelector(".button9").addEventListener("click", function () { playMp3(alle[7]) });
-
-//7.2//
-
- document.querySelector(".playbutton").addEventListener("click", function () { playbeat() });
-
- function playbeat() {
-
-    setTimeout(function () {
-        playMp3(alle[5]);
-    }, 500)
-
-    setTimeout(function () {
-        playMp3(alle[4]);
-    }, 750)
-
-    setTimeout(function () {
-        playMp3(alle[8]);
-    }, 1000)
-
-    setTimeout(function () {
-        playMp3(alle[5]);
-    }, 1250)
-
-    setTimeout(function () {
-        playMp3(alle[4]);
-    }, 1500)
-
-    setTimeout(function () {
-        playMp3(alle[8]);
-    }, 2000)
-
-    setTimeout(function () {
-        playMp3(alle[7]);
-    }, 2250)
-
-}
+document.querySelector(".button1").addEventListener("click", function (): void { playMp3(alle[0]) });
+document.querySelector(".button2").addEventListener("click", function (): void { playMp3(alle[1]) });
+document.querySelector(".button3").addEventListener("click", function (): void { playMp3(alle[2]) });
+document.querySelector(".button4").addEventListener("click", function (): void { playMp3(alle[3]) });
+document.querySelector(".button5").addEventListener("click", function (): void { playMp3(alle[4]) });
+document.querySelector(".button6").addEventListener("click", function (): void { playMp3(alle[5]) });
+document.querySelector(".button7").addEventListener("click", function (): void { playMp3(alle[8]) });
+document.querySelector(".button8").addEventListener("click", function (): void { playMp3(alle[6]) });
+document.querySelector(".button9").addEventListener("click", function (): void { playMp3(alle[7]) });
 });
 
+ // playbutton//
+ document.querySelector(".playbutton").addEventListener("click", function (): void { playbeat()});
+
+ function playbeat(): void {
+     loop = setInterval(function (): void {
+    setTimeout(function (): void { playMp3(alle[5]);}, 500)
+    setTimeout(function (): void { playMp3(alle[4]);}, 750)
+    setTimeout(function (): void { playMp3(alle[8]);}, 1000)
+ }, 1000);
+}
+
+//random beat//
+document.querySelector(".random").addEventListener("click", function () {random(); });
+
+function random(): void {
+    randomButton = setInterval(function(): void {
+    playMp3(alle[Math.floor(Math.random() * 9)]);
+    console.log (randomButton);
+},                             500);
+}
+
+//delete button//
+document.querySelector(".delete").addEventListener("click", function () {deleteRemix(); });
+
+function deleteRemix(): void {
+    clearInterval(randomButton);
+    clearInterval(loop);
+}
+
+//play/stop funktion//
+playBtn.addEventListener("click", function (): void {
+    changeClasses(this, stopBtn);
+});
+
+stopBtn.addEventListener("click", function (): void {
+    changeClasses(this, playBtn);
+    clearInterval(loop);
+    indexZahler = 0;
+});
+
+
+function changeClasses(firsHTMLElement: HTMLElement, secondHTMLElement: HTMLElement): void {
+    firsHTMLElement.classList.add("isHidden");
+    secondHTMLElement.classList.remove("isHidden");
+
+
+}
